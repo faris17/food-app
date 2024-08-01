6a9b9578be43d3fe02189d3aa16416d47e524f88
faris17 <fzain712@gmail.com>
2024-08-01 17:18:01 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Food App
+
+Latihan untuk membuat Food App dengan Vue 3
+
+Tutorial Youtube:
+[Vue 3 Untuk Pemula](https://www.youtube.com/playlist?list=PLSrCeSrrFkMXSfL64lQ5fFxXZ2HMvZCQV)
+
+Course Laravel Inertia Vue,
+[Membangun E-Commerce](https://muliastudy.com/course/show/membuat-e-commerce-dengan-filament-inertia-vue)
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!--Navbar-->
+    <Navbar />
+    <!--end Navbar-->
+
+    <!-- Content Section -->
+    <RouterView />
+    <!-- End Content-->
+
+    <!-- Footer Section -->
+    <Footer />
+    <!--End Footer-->
+  </div>
+</template>
+
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Category from './components/Category.vue';
+import Footer from './components/Footer.vue';
+import ListFoods from './pages/ListFoods.vue';
+</script>
